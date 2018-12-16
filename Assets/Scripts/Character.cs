e676e83e13f2c9548fcff5c6a7dd99322e16b776
votalub <arthur.bullatov@gmail.com>
2018-12-17 02:30:01 +0300

    Initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character : Unit
+{
+    [SerializeField]
+    private int lives = 5;
+
+    public int Lives
+    {
+        get { return lives; }
+        set
+        {
+            if (value < 5) lives = value;
+            livesBar.Refresh();
+        }
+    }
+    private LivesBar livesBar;
+
+
+    [SerializeField]
+    private float speed = 3.0F;
+    [SerializeField]
+    private float jumpForce = 15.0F;
+
+    private bool isGrounded = false;
+
+    private Bullet bullet;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+              
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        livesBar = FindObjectOfType<LivesBar>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+
+        bullet = Resources.Load<Bullet>("Bullet");
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        ReloadLevel();
+        if (isGrounded) State = CharState.Idle;
+
+        if (Input.GetButtonDown("Fire2")) Shoot();
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+    }
+    
+    private void ReloadLevel()
+    {
+        if (Lives <= 0)// если жизней меньше либо равно нулю
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+
+        sprite.flipX = direction.x < 0.0F;
+
+        if (isGrounded) State = CharState.Run;
+    }
+
+    private void Jump()
+    {
+   
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Shoot()
+    {
+        Vector3 position = transform.position;  position.y += 1.0F;
+        Bullet newBullet = Instantiate(bullet, position, bullet.transform.rotation);
+
+        newBullet.Parent = gameObject;
+        newBullet.Direction = newBullet.transform.right * (sprite.flipX ? -1.0F : 1.0F);
+    }
+
+    public override void ReceiveDamage()
+    {
+        Lives--;
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(transform.up * 8.0F, ForceMode2D.Impulse);
+
+        Debug.Log(lives);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+
+        isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = CharState.Jump;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        Bullet bullet = collider.gameObject.GetComponent<Bullet>();
+        if (bullet && bullet.Parent != gameObject)
+        {
+            ReceiveDamage();
+        }
+
+        if (collider.gameObject.tag == "Finish")
+        {
+            Application.LoadLevel("Scene2");
+        }
+    }
+}
+
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
+}
---
+++ Assets/Scripts/ControlSceneAndMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSceneAndMenu : MonoBehaviour {
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+
+    public void ShowExitButtons()   // функция которая будет вызываться нажатием кнопки выхода
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+    }
+    public void BackInMenu()   // возврат в главное меню
+    {
+        buttonsMenu.SetActive(true);
+        buttonsExit.SetActive(false);
+    }
+    public void ExitGame()   // возврат в главное меню
+    {
+        Application.Quit();
+    }
+
+   public void NewGameLoadScene()   // возврат в главное меню
+    {
+        Application.LoadLevel("SampleScene");
+    } 
+
+}
