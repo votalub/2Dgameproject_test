82fb517b792cd03d7171b57b9f79ba3ed812a58d
votalub <44325780+votalub@users.noreply.github.com>
2018-12-17 05:04:12 +0300

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,6 +1,42 @@
 #! /bin/sh
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+BASE_URL=https://beta.unity3d.com/download/46dda1414e51
+VERSION=2017.2.0f3
+
+UNITY_OSX_PACKAGE="MacEditorInstaller/Unity-$VERSION.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE="MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+
+
+download() {
+	
+	FILE=$1
+	URL="$BASE_URL/$FILE"
+
+	#download package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` ] ; then
+		echo "$FILE does not exist. Downloading from $URL: "
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` "$URL"
+	else
+		echo "$FILE Exists. Skipping download."
+	fi
+}
+
+install() {
+	PACKAGE=$1
+	download "$PACKAGE"
+	
+	echo "Installing "`basename "$PACKAGE"`
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE"` -target /
+}
+
+
+
+echo "Contents of Unity Download Cache:"
+ls "$UNITY_DOWNLOAD_CACHE"
+
+echo "Installing Unity..."
+install "$UNITY_OSX_PACKAGE"
+install "$UNITY_WINDOWS_TARGET_PACKAGE"staller -dumplog -package `basename "$package"` -target /
+}
